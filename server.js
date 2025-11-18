556b2b6876d73336744944a7e0e63009fbbce531
B yss <20240031277@my.xu.edu.ph>
2025-11-18 21:27:04 +0800

    missed file name edit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import uploadRoute from "./routes/upload.js";
+import uploadRoute from "./routes/uploadRoutes.js";
 
 dotenv.config();
 

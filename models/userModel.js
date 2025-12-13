0cb903e7d8eee2438ebc2d817ed54a95d0de4788
B yss <20240031277@my.xu.edu.ph>
2025-12-13 20:48:30 +0800

    email field missing fixed

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    passwordResetToken: String,
+    passwordResetExpires: Date
   },
   { timestamps: true }
 );
